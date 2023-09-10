3b3ad37a53116b535d2951bbeff8ccd024025c1b
NiaToloraia <137307256+NiaToloraia@users.noreply.github.com>
2023-09-10 16:54:50 +0400

    circle color changes

--- main.js
+++ main.js
@@ -17,10 +17,7 @@ const planetBtn = document.querySelector('#planet-btn');
 
 
 
-burgerBtn.addEventListener('click', () => {
-    planetBtn.classList.toggle('active');
-    burgerBtn.classList.toggle('active');
-});
+
 
 
 const PLANET_API = 'https://planets-api.vercel.app/api/v1/planets';
@@ -29,14 +26,17 @@ for (let i = 0; i <  planetLinks.length; i++) {
     planetLinks[i].addEventListener('click', () => {
         selectPlanet(planetLinks[i].textContent);
 
-        for (let j = 0; j < planetLinks.length; j++) {
-            planetLinks[j].classList.add('planetLine');
-        }
-        planetLinks[i].classList.remove('planetLine');
+        // for (let j = 0; j < planetLinks.length; j++) {
+        //     planetLinks[j].classList.add('planetLine');
+        // }
+        // planetLinks[i].classList.remove('planetLine');
 
 
     });
 }
+
+
+
 
 
 
@@ -50,29 +50,42 @@ const selectPlanet = async (planet = 'Mercury') => {
     rotationN.textContent = data.rotation;
     revolutionN.textContent = data.revolution;
     radiusN.textContent = data.radius;
-    temperatureN .textContent = data.temperature;
+    temperatureN.textContent = data.temperature;
+
    
 
     overviewFirst.addEventListener('click', () => {
         infoP.textContent = data.overview.content;
         planetImg.src = data.images.planet;
         planetImgAdd.style.display = 'none';
+
     });
 
     internalSec.addEventListener('click', () => {
         infoP.textContent = data.structure.content;
         planetImg.src = data.images.internal;
         planetImgAdd.style.display = 'none';
+
     });
 
     geologyThird.addEventListener('click', () => {
         infoP.textContent = data.geology.content;
         planetImg.src = data.images.planet;
         planetImgAdd.src = data.images.geology;
-        planetImgAdd.style.display = 'block';
+
        
     });
 
 };
 
 selectPlanet();
+
+
+burgerBtn.addEventListener('click', () => {
+    planetBtn.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
+
+});
+
+
+   
